a17775b5bfd9f405fbc12d5303f13bc278503649
mofeimw <mofei.mw@gmail.com>
2022-06-27 03:34:19 -0500

    display timestamps as formatted dates

--- js/table.js
+++ js/table.js
@@ -32,6 +32,21 @@ function parse() {
     // loop back through and add all the data
     JSON.forEach(function (obj, z) {
         for (let key in obj) {
+            // check if the data is a number
+            if (!isNaN(obj[key])) {
+                // check if it is an epoch (?) timestamp
+                // 13 digits = milliseconds
+                // 10 digits = seconds
+                if (obj[key].toString().length == 13 || obj[key].toString().length == 10) {
+                    // make a new JavaScript Date
+                    // and convert to the local timezone date
+                    obj[key] = new Date(obj[key]).toLocaleDateString();
+                // never
+                } else if (obj[key] == 0) {
+                    obj[key] = "never"
+                }
+            }
+
             DATA[key].push(obj[key]);
         }
     });
